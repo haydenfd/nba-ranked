3176bd07fc4af18f86cd36b75eebdcdde3e9a914
Hayden D’Souza <haydenfds@gmail.com>
2024-07-20 18:33:09 -0700

    Added initial modal content

--- src/Components/Modals/StatsModal.tsx
+++ src/Components/Modals/StatsModal.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import {Modal, ModalContent, ModalHeader, ModalBody, Button, useDisclosure} from "@nextui-org/react";
-import { QuestionMarkCircledIcon } from "@radix-ui/react-icons";
+import { BarChartIcon } from "@radix-ui/react-icons";
 
-
-export function GuideModal() {
+export function StatsModal() {
   const {isOpen, onOpen, onOpenChange} = useDisclosure();
 
   return (
@@ -12,11 +11,11 @@ export function GuideModal() {
           color="default"
           className="text-medium px-2 md:px-6 md:py-2 rounded-full md:rounded-lg bg-transparent border-2 border-transparent hover:border-white transition ease-in-out delay-150"
           startContent={
-            <QuestionMarkCircledIcon className="scale-[150%] text-white md:mr-2" />
+            <BarChartIcon className="scale-[150%] text-white md:mr-2" />
           }
           onClick={onOpen}
         >
-          <span className="hidden md:inline text-xl text-white">Guide</span>
+          <span className="hidden md:inline text-xl text-white">Statistics</span>
         </Button>      
         <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top">
         <ModalContent>
